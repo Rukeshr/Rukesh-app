fb6e8fb4721c64d802c3b19ab7cb8de87d9ccb5f
Rukesh R <Rukeshr16@gmail.com>
2020-10-12 18:05:18 +0530

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :welcome
   root 'welcome#index'
-  get 'about', to: 'welcome#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
